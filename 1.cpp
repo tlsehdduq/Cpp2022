7917e29c892bbc9c39cffd7c9bb218857c5151b4
tlsehdduq <bmbc98@naver.com>
2022-03-23 00:36:06 +0900

    2022 03 23 00:35

--- 1.cpp
+++ 1.cpp
@@ -2,46 +2,41 @@
 //                                                            3주 2일차                       
 //
 //공부할 내용  - 
-// C++은 자료를 어떻게 정렬하는가?
-// *, & - 원격조작 기능
-// 함수란?
+//	const의 의미 - 메모리를 읽을 수는 있지만 쓸 수는 없다. 
+// void* - 메모리에 있는 원소의 자료형이 무엇인지 모를 때 사용한다.
 // 
 //-------------------------------------------------------------------------------------------------
 
 #include<iostream>
 #include<algorithm>
+#include<random>
 #include"save.h"
 using namespace std;
-int f(const void*, const void*); //void 형식을 모르겠단 의미
 
-int f(const void* a, const void* b) //const        
-{
-	int* p = (int*)a;
-	int* q = (int*)b;
-
-	if (*p < *q)
-		return -1;
-
-	else if (*p > *q)
-		return 1;
-
-	return 0;
-}
+default_random_engine dre;
+uniform_int_distribution<int>uid{1, 1000};
 
 int main()
 {	
-	int a[]{ 3,1,2};
-	//오름차순으로 정렬하시오.:
-	//주의 - 이 코드는 STL시간에 배울것임 지금은 감상만하자.
-	// 
-	//qsort 함수는 메모리를 이해하는데 매우 좋은 함수이다. 시간내서 알아두자.
-	//qsort 함수는 어떤자료든 정렬하는 만능함수이다.
+	int a[1000];
+	for (int& num : a)
+		num = uid(dre);
 
-	qsort(a, size(a), sizeof(int), f);			//[]만 정렬 가능 
-	
-	for (int n : a)
-		cout << n << " ";
+	cout << "원래 데이터" << endl;
+
+	for (int num : a)
+		cout << num << " ";
+	cout << endl;
 	cout << endl;
 
+	qsort(a, size(a), sizeof(int), [](const void* a, const void* b) {
+		return *(int*)a - *(int*)b;
+		});
+	cout << "오름차순 정렬 후 " << endl;
+	cout << endl;
+
+	for (int num : a)
+		cout << num << " ";
+	cout << endl << endl;
 	save("1.cpp");
 }
